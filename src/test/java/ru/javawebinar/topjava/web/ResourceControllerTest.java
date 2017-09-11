bb47c694903aefede11bb1ea8641878c4a5d5d2e
rbezbabnov <rbezbabnov@at-conulting.ru>
2017-09-11 18:19:36 +0300

    7 13 soapui utf8 converter

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.Test;
+
+public class ResourceControllerTest extends AbstractControllerTest {
+  @Test
+  public void testStyle(){
+   // mockMvc.perform("/")
+  }
+}
