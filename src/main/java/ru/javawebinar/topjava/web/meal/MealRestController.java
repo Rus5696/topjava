37e48173193bd0143a45ec3bceae845c56196bee
rbezbabnov <rbezbabnov@at-conulting.ru>
2017-09-11 18:56:07 +0300

    meal rest controller

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,7 +1,57 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.stereotype.Controller;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealWithExceed;
 
-@Controller
+@RestController
+@RequestMapping(MealRestController.REST_URL)
 public class MealRestController extends AbstractMealController {
+
+  static final String REST_URL = "/rest/meals";
+
+  @Override
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public List<MealWithExceed> getAll() {
+    return super.getAll();
+  }
+
+  @Override
+  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Meal get(@PathVariable("id") int id) {
+    return super.get(id);
+  }
+
+  @Override
+  @DeleteMapping(value = "/{id}")
+  public void delete(int id) {
+    super.delete(id);
+  }
+
+  @Override
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Meal create(@RequestBody Meal meal) {
+    return super.create(meal);
+  }
+
+  @Override
+  public void update(Meal meal, int id) {
+    super.update(meal, id);
+  }
+
+  @Override
+  public List<MealWithExceed> getBetween(LocalDate startDate, LocalTime startTime,
+      LocalDate endDate, LocalTime endTime) {
+    return super.getBetween(startDate, startTime, endDate, endTime);
+  }
 }
--- src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -1,10 +1,17 @@
 package ru.javawebinar.topjava.web;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 
 public class ResourceControllerTest extends AbstractControllerTest {
   @Test
-  public void testStyle(){
-   // mockMvc.perform("/")
+  public void testStyle() throws Exception {
+    mockMvc.perform(get("/resources/css/style.css"))
+        .andDo(print())
+        .andExpect(status().isOk()).andExpect(content().contentType("text/css"));
   }
 }
